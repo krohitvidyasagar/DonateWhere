18608337fd672035748a999f8afcdb612e914c55
krohitvidyasagar <k.rohitvidyasagar@gmail.com>
2024-04-17 10:38:45 -0500

    Authentication and registration api added

---
+++ donation/views.py
@@ -0,0 +1,70 @@
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import generics
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.response import Response
+
+from donation.models import User
+from donation.serializers import UserLoginSerializer
+from donation.service import AuthenticationUtils
+
+
+class UserRegistrationView(generics.CreateAPIView):
+    name = 'user-registration-view'
+    queryset = User.objects.all()
+    authentication_classes = []
+
+    def post(self, request, *args, **kwargs):
+        email = self.request.data.get('email')
+
+        # Check if the user already exists in the database
+        try:
+            user = User.objects.get(email=email)
+
+        except ObjectDoesNotExist:
+            name = self.request.data.get('name')
+
+            phone = self.request.data.get('phone')
+            password = self.request.data.get('password')
+
+            hashed_password = AuthenticationUtils.get_hashed_password(password.encode('utf-8'))
+
+            user = User.objects.create(name=name, email=email.lower(), password=hashed_password)
+
+            if phone:
+                user.phone = phone
+                user.save()
+
+            return Response({'detail': 'User has been successfully registered'})
+
+
+class LoginView(generics.CreateAPIView):
+    name = 'user-login-view'
+    queryset = User.objects.all()
+    authentication_classes = []
+
+    def post(self, request, *args, **kwargs):
+        email = self.request.data['email']
+        password = self.request.data['password']
+
+        try:
+            authenticated_user = User.objects.get(email__iexact=email)
+
+            hashed_password = AuthenticationUtils.get_hashed_password(password.encode('utf-8'))
+
+            if str(hashed_password) != authenticated_user.password:
+                raise ObjectDoesNotExist()
+
+            user_claims = AuthenticationUtils.get_user_claims(authenticated_user)
+
+            access = AuthenticationUtils.get_user_access_token(user_claims)
+            refresh = AuthenticationUtils.get_user_refresh_token(user_claims)
+
+            return Response({
+                'user': UserLoginSerializer(authenticated_user).data,
+                'access': access,
+                'refresh': refresh
+
+            })
+
+        except ObjectDoesNotExist:
+            raise AuthenticationFailed(detail='Authentication Error')
