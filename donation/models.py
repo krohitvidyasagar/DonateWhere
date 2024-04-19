d9ab3b3ad3d8ae9b85e50ce8b3c330773c0db55b
krohitvidyasagar <k.rohitvidyasagar@gmail.com>
2024-04-19 18:28:12 -0500

    Migration files added for donations

--- donation/models.py
+++ donation/models.py
@@ -1,14 +1,24 @@
 import uuid
+from enum import Enum
 
 from django.db import models
 
 
+class UserType(Enum):
+    PERSONAL = 'PERSONAL'
+    ORGANIZATION = 'ORGANIZATION'
+
+
 class User(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    name = models.CharField(max_length=100, null=False)
+    first_name = models.CharField(max_length=30, null=True)
+    last_name = models.CharField(max_length=40, null=True)
+
     email = models.EmailField(max_length=100, null=False, unique=True)
     phone = models.CharField(max_length=20, null=True)
     password = models.CharField(max_length=100, null=False)
+    user_type = models.CharField(max_length=20, null=False, default=UserType.PERSONAL.value)
+    address = models.TextField(null=True)
 
     profile_photo_base64 = models.TextField(null=True)
 
@@ -17,4 +27,21 @@ class User(models.Model):
 
     class Meta:
         db_table = 'user'
+        ordering = ['-created_at']
+
+
+class Donation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+
+    donated_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.CharField(max_length=100, null=False)
+    category = models.CharField(max_length=50, null=False)
+    datetime = models.DateTimeField(null=False)
+    is_claimed = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'donation'
         ordering = ['-created_at']
