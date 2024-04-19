8bd19f04fbd6efd0c078ad69a0156415595482fd
krohitvidyasagar <k.rohitvidyasagar@gmail.com>
2024-04-19 10:48:54 -0500

    Secret key removed

--- donate_where/settings.py
+++ donate_where/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8K8LzY1VerZqcx3xxeJrbxl3nfUcv25E'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
