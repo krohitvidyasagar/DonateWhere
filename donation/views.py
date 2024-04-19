213ec10649f42b4bf5c13cb0aa8132481efc1b4a
krohitvidyasagar <k.rohitvidyasagar@gmail.com>
2024-04-19 18:29:09 -0500

    Profile API added

--- donation/views.py
+++ donation/views.py
@@ -1,10 +1,10 @@
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import generics
-from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.exceptions import AuthenticationFailed, ParseError
 from rest_framework.response import Response
 
 from donation.models import User
-from donation.serializers import UserLoginSerializer
+from donation.serializers import UserLoginSerializer, UserProfileSerializer
 from donation.services import AuthenticationUtils
 
 
@@ -20,18 +20,27 @@ class UserRegistrationView(generics.CreateAPIView):
         try:
             user = User.objects.get(email=email)
 
+            raise ParseError(detail='User with these email address already exists')
+
         except ObjectDoesNotExist:
-            name = self.request.data.get('name')
+            first_name = self.request.data.get('first_name')
+            last_name = self.request.data.get('last_name')
 
             phone = self.request.data.get('phone')
             password = self.request.data.get('password')
+            user_type = self.request.data.get('user_type')
 
             hashed_password = AuthenticationUtils.get_hashed_password(password.encode('utf-8'))
 
-            user = User.objects.create(name=name, email=email.lower(), password=hashed_password)
+            user = User.objects.create(first_name=first_name, last_name=last_name,
+                                       email=email.lower(), password=hashed_password)
 
             if phone:
                 user.phone = phone
+                user.save()
+
+            if user_type:
+                user.user_type = user_type
                 user.save()
 
             return Response({'detail': 'User has been successfully registered'})
@@ -68,3 +77,15 @@ class LoginView(generics.CreateAPIView):
 
         except ObjectDoesNotExist:
             raise AuthenticationFailed(detail='Authentication Error')
+
+
+class ProfileView(generics.ListAPIView):
+    name = 'user-profile-view'
+    queryset = User.objects.all()
+    serializer_class = UserProfileSerializer
+
+    def get(self, request, *args, **kwargs):
+        email = self.request.auth_context['user']
+        user = User.objects.get(email=email)
+
+        return Response(self.get_serializer(user).data)
