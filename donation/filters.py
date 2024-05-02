ec1d82ec46860cffdcbe25b08305d0adfdd7f711
krohitvidyasagar <k.rohitvidyasagar@gmail.com>
2024-05-02 13:41:41 -0500

    Event filters added

--- donation/filters.py
+++ donation/filters.py
@@ -7,3 +7,14 @@ class DonationFilter(filters.FilterSet):
     description = filters.CharFilter(field_name='description', lookup_expr='icontains')
     address = filters.CharFilter(field_name='address', lookup_expr='icontains')
     is_claimed = filters.BooleanFilter(field_name='is_claimed')
+
+
+class EventFilter(filters.FilterSet):
+    name = filters.CharFilter(field_name='name', lookup_expr='icontains')
+    description = filters.CharFilter(field_name='description', lookup_expr='icontains')
+    address = filters.CharFilter(field_name='address', lookup_expr='icontains')
+    organization = filters.CharFilter(field_name='organization__first_name', lookup_expr='icontains')
+
+    datetime_gte = filters.DateFilter(field_name='datetime', lookup_expr='gte')
+    datetime_lte = filters.DateFilter(field_name='datetime', lookup_expr='lte')
+
